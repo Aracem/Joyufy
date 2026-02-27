5cc1e55af9045f08be1b501b712daa36ec33908b
Marcos Trujillo <marcostruji@gmail.com>
2026-02-27 15:02:24 +0100

    chore: bump version to 1.0.2

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -69,7 +69,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "Joyufy"
-            packageVersion = "1.0.1"
+            packageVersion = "1.0.2"
             modules("java.sql", "java.naming")
             description = "Control personal de finanzas"
             copyright = "© 2026 Aracem"
